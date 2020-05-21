1c1e73ebe87d16b73f985622c6dd5f39f9621374
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-20 20:19:43 -0600

    Create set up and test it exists

--- test/pet_test.rb
+++ test/pet_test.rb
@@ -1,27 +1,50 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/pet'
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/pet"
 
 class PetTest < Minitest::Test
+
+  def setup
+    @samson = Pet.new({name: "Samson", type: :dog, age: 3})
+  end
+
   def test_it_exists
-    samson = Pet.new({name: "Samson", type: :dog})
-    assert_instance_of Pet, samson
-  end
-
-  def test_it_has_attributes
-    samson = Pet.new({name: "Samson", type: :dog})
-    assert_equal "Samson", samson.name
-    assert_equal :dog, samson.type
-  end
-
-  def test_it_is_not_fed_by_default
-    samson = Pet.new({name: "Samson", type: :dog})
-    assert_equal false, samson.fed?
-  end
-
-  def test_it_can_be_fed
-    samson = Pet.new({name: "Samson", type: :dog})
-    samson.feed
-    assert_equal true, samson.fed?
+    assert_instance_of Pet, @samson
   end
 end
+
+
+
+
+
+
+
+
+
+# require 'minitest/autorun'
+# require 'minitest/pride'
+# require './lib/pet'
+#
+# class PetTest < Minitest::Test
+#   def test_it_exists
+#     samson = Pet.new({name: "Samson", type: :dog})
+#     assert_instance_of Pet, samson
+#   end
+#
+#   def test_it_has_attributes
+#     samson = Pet.new({name: "Samson", type: :dog})
+#     assert_equal "Samson", samson.name
+#     assert_equal :dog, samson.type
+#   end
+#
+#   def test_it_is_not_fed_by_default
+#     samson = Pet.new({name: "Samson", type: :dog})
+#     assert_equal false, samson.fed?
+#   end
+#
+#   def test_it_can_be_fed
+#     samson = Pet.new({name: "Samson", type: :dog})
+#     samson.feed
+#     assert_equal true, samson.fed?
+#   end
+# end
